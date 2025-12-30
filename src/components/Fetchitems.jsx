124a38b4b6f769ff470284622f52e104c561f9c4
being-shivalika <shivalika271@gmail.com>
2025-12-30 21:00:50 +0530

    .

--- src/components/Fetchitems.jsx
+++ src/components/Fetchitems.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { itemsActions } from "../store/itemsSlice";
-import { fetchStatusActions } from "../store/fetchStatusSlice";
+import { fetchStatusActions } from "../Store/fetchStatusSlice";
 
 const FetchItems = () => {
   const fetchStatus = useSelector((store) => store.fetchStatus);
